a6f0c6fbb8707533f21f38f8504c056db66adfcf
Sarmad Ajmal <24247576+sarmad-ajmal@users.noreply.github.com>
2023-01-27 20:17:25 +0400

    add prettier in project

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App';
+import App from './App'
 
 const Root = () => {
-
   return (
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  );
+  )
 }
 
 // If you want to start measuring performance in your app, pass a function
