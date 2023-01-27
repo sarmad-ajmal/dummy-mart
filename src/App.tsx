3d78c11b8a082c72584c9c3913e8ae171ac2c14a
Sarmad Ajmal <24247576+sarmad-ajmal@users.noreply.github.com>
2023-01-28 03:22:13 +0400

    add dummy login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
+
+const LoginPage = lazy(() =>
+  import('./components/auth').then(module => ({ default: module.LoginPage })),
+)
 
 function App() {
-  return <div className='App'>hello world</div>
+  return (
+    <div className='App'>
+      <Router>
+        <Suspense
+          fallback={() => {
+            ;<div>loading</div>
+          }}
+        >
+          <Routes>
+            <Route path='*' element={<LoginPage />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/auth/login/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import useLogin from './index.hook'
+
+const Login = () => {
+  const {} = useLogin()
+  return <div>Login Page</div>
+}
+export default Login
